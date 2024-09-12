755fb62900dcac427aa9c8d3e5031e7993fe4aef
Laturas <laturaas@gmail.com>
2024-09-12 12:41:58 +0300

    Almost done

--- testcases/testcase_001_answer.s
+++ testcases/testcase_001_answer.s
@@ -1,9 +1,10 @@
 	.globl	__main
 __main:
 	movl   $0x1, %eax
- 	not    %eax
  	addl   $0x1, %eax
  	addl   $0x1, %eax
  	addl   $0x1, %eax
- 	addl   $0x1, %eax
+	movl   $0x1, %ebx
+ 	not    %ebx
+ 	addl   %ebx, %eax
  	ret
